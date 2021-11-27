a72507831bbe50fcc97e7581adacb776643a459f
Barry Frost <public@barryfrost.com>
2021-11-27 20:11:18 +0000

    Rename store as data

--- src/scheduled/fetch/index.js
+++ src/scheduled/fetch/index.js
@@ -1,9 +1,12 @@
 const fetch = require('./fetch')
 const parse = require('./parse')
-const store = require('./store')
+const data = require('./data')
 const micropub = require('./micropub')
 
 exports.handler = async function scheduled (event) {
+  // const users = data.getUsers()
+  // for (user of users) {
+  // }
   const url = 'http://localhost:3333/'
   const endpoint = 'http://localhost:3333/micropub'
   const token = 'foo'
@@ -11,10 +14,10 @@ exports.handler = async function scheduled (event) {
 
   const feed = await fetch(letterboxdUser)
   const item = await parse.getFirstItem(feed)
-  const lastGuid = await store.getUserLastGuid(url)
+  const lastGuid = await data.getUserLastGuid(url)
   const itemGuid = item.guid[0]._
   if (itemGuid !== lastGuid) {
     const ok = await micropub.post(endpoint, token, item)
-    if (ok) await store.updateUserLastGuid(url, itemGuid)
+    if (ok) await data.updateUserLastGuid(url, itemGuid)
   }
 }
